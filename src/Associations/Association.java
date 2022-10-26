fb5e10c59d9e38e610f15b4d77911d845cc9ba6b
jalbertomr <jalbertomr@yahoo.com>
2022-10-26 12:48:57 -0500

    added Association ...

--- src/Associations/Association.java
+++ src/Associations/Association.java
@@ -6,6 +6,40 @@ import java.util.Arrays;
 public class Association {
     public static void main(String[] args) {
 
+        Person personA = new Person("Hugo", 7);
+        Person personB = new Person( "Paco", 8);
+        Person siamesA = new Person( "SiamesA", 5);
+        Person siamesB = new Person("SiamesB", 5);
+
+        // Association
+        // Person ----------- Person    bidirectional association, both has his own life cycle.
+        personA.setConnectWith( personB);
+        personB.setConnectWith( personA);
+        // Person <>------------<> Person      Not used  BiAggregation
+        // siamesA ◄►-----------◄► siamesB     Not used  BiComposition
+
+        // Flight ----------- Plane     bidirectional association, but is most an Aggregation in "use" but a Opposite Composition in Life
+        // Flight <>--------- Plane     Flight "uses-a" Plane, If the Flight dies the Plane Not, If the Plane die the Flight die
+        // Flight <>-------◄► Plane     Flight "uses-a" Plane, Plane "has-a" Flight, Flight dies the plane Not, Plane dies then Flight dies.
+
+        // Aggregation
+        // PersonA <>-------- PersonB   This is an Aggregation, personA -- "has-a" | "uses-a" -- personB, both has his own life cycle.
+        personA.setConnectWith( personB);
+
+        // PersonA --------<> PersonB   This is an Aggregation, personB -- "has-a" | "uses-a" -- personA, both has his own life cycle.
+        personB.setConnectWith( personA);
+
+        // SiamesA ◄►-------- SiamesB  This is a Composition, SiamesA -- "has-a" | "uses-a" -- personB, SiamesA determines the both life cycles.
+        siamesA.setConnectWith( siamesB);
+
+        // SiamesA --------◄► SiamesB  This is a Composition,  SiamesB -- "has-a" | "uses-a" -- personA, SiamesB determines the both life cycles.
+        siamesB.setConnectWith( siamesA);
+
+        // SiamesA ◄►------◄► SiamesB  This is a Composition Not used.
+        siamesA.setConnectWith( siamesB);
+        siamesB.setConnectWith( siamesA);
+
+
         // Driver ----------- Vehicle
         // both has independent life cycle
         // Driver "Has a" or "Uses a" Vehicle
---
+++ src/Associations/Person.java
@@ -0,0 +1,31 @@
+package Associations;
+
+import java.util.Date;
+
+public class Person {
+    String name;
+    Integer age;
+    Person connectWith;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person getConnectWith() {
+        return connectWith;
+    }
+
+    public void setConnectWith(Person connectWith) {
+        this.connectWith = connectWith;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", connectWith=" + connectWith +
+                '}';
+    }
+}
